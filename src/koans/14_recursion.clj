c1f55d56f47e822dc40770838c82c9b15f83ee36
Thammachart <1731496+Thammachart@users.noreply.github.com>
2019-10-26 18:39:44 +0700

    koans 13-15 finished

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -2,22 +2,31 @@
   (:require [koan-engine.core :refer :all]))
 
 (defn is-even? [n]
-  (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+  (cond
+    (= n 0) true
+    (= n 1) false
+  :else (is-even? (- n 2))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc '()]
+    (if (empty? coll)
+      acc
+      (recur (rest coll) (cons (first coll) acc)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (<= n 1)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
